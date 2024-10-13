8a81e191c758dc1faf4e53e6542a14492eb3e326
erzahoq <iamethankwan@gmail.com>
2024-10-12 19:27:45 -0700

    that is a lot of text just for checking

--- commands/userstatistics/compare.js
+++ commands/userstatistics/compare.js
@@ -17,10 +17,161 @@ module.exports = {
                 .setRequired(true),
         ),
 	async execute(interaction) {
+        await interaction.deferReply({ephemeral: true}) // but here's the thinker
+
         const user1 = interaction.options.getString('user1').toLowerCase();
         const user2 = interaction.options.getString('user2').toLowerCase();
 
-        await interaction.reply(`${user1}, ${user2}`);
+        let userStats1;
+        let userStats2;
+
+        let response = null;
+
+        const discordRegex = new RegExp('<@[0-9]+>');
+
+        //calls api 4 times in the worst case sceneario
+        //thats probably bad
+        
+        //user one
+        if (discordRegex.test(user1)) {
+            mention = user1.slice(2, -1);
+
+            if (mention.startsWith('!')) {
+                mention = mention.slice(1);
+            }
+
+            const user = interaction.client.users.cache.get(mention);
+            if (!user) {
+                return await interaction.editReply({
+                    content: 'User 1 could not be found on discord!',
+                    ephemeral: true
+                });
+            }
+
+            console.log(user)
+
+            response = await fetch(`https://ch.tetr.io/api/users/search/discord:${user.id}`);
+            let stats = await response.json();
+
+            console.log(stats)
+
+            if (!stats.data) {
+                return await interaction.editReply({
+                    content: 'User 1 has not linked a TETR.IO account!',
+                    ephemeral: true
+                });
+            }
+            
+            response = await fetch(`https://ch.tetr.io/api/users/${stats.data.user._id}`);
+
+            userStats1 = await response.json();
+
+            if (!userStats1.success) { // not sure if this one can even happen but um, just in case :SILENCE:
+                if (userStats1.error.msg === "No such user! | Either you mistyped something, or the account no longer exists.") {
+                    return await interaction.editReply({
+                        content: 'Could not find user 1! Either you mistyped something, or this user no longer exists.',
+                        ephemeral: true
+                    });
+                } else {
+                    return await interaction.editReply({
+                        content: 'I had an issue accessing the TETR.IO servers! Please try again later.',
+                        ephemeral: true
+                    });
+                }
+            }
+
+        } else {
+            response = await fetch(`https://ch.tetr.io/api/users/${user1}`);
+
+            userStats1 = await response.json();
+
+            if (!userStats1.success) {
+                if (userStats1.error.msg === "No such user! | Either you mistyped something, or the account no longer exists.") {
+                    return await interaction.editReply({
+                        content: 'Could not find user 1! Either you mistyped something, or this user no longer exists.',
+                        ephemeral: true
+                    });
+                } else {
+                    return await interaction.editReply({
+                        content: 'I had an issue accessing the TETR.IO servers! Please try again later.',
+                        ephemeral: true
+                    });
+                }
+            }
+        }
+
+        //user two
+        if (discordRegex.test(user2)) {
+            mention = user1.slice(2, -1);
+
+            if (mention.startsWith('!')) {
+                mention = mention.slice(1);
+            }
+
+            const user = interaction.client.users.cache.get(mention);
+            if (!user) {
+                return await interaction.editReply({
+                    content: 'User 2 could not be found on discord!',
+                    ephemeral: true
+                });
+            }
+
+            console.log(user)
+
+            response = await fetch(`https://ch.tetr.io/api/users/search/discord:${user.id}`);
+            let stats = await response.json();
+
+            console.log(stats)
+
+            if (!stats.data) {
+                return await interaction.editReply({
+                    content: 'User 2 has not linked a TETR.IO account!',
+                    ephemeral: true
+                });
+            }
+            
+            response = await fetch(`https://ch.tetr.io/api/users/${stats.data.user._id}`);
+
+            userStats2 = await response.json();
+
+            if (!userStats2.success) { // not sure if this one can even happen but um, just in case :SILENCE:
+                if (userStats2.error.msg === "No such user! | Either you mistyped something, or the account no longer exists.") {
+                    return await interaction.editReply({
+                        content: 'Could not find user 2! Either you mistyped something, or this user no longer exists.',
+                        ephemeral: true
+                    });
+                } else {
+                    return await interaction.editReply({
+                        content: 'I had an issue accessing the TETR.IO servers! Please try again later.',
+                        ephemeral: true
+                    });
+                }
+            }
+
+        } else {
+            response = await fetch(`https://ch.tetr.io/api/users/${user2}`);
+
+            userStats2 = await response.json();
+
+            if (!userStats2.success) {
+                if (userStats2.error.msg === "No such user! | Either you mistyped something, or the account no longer exists.") {
+                    return await interaction.editReply({
+                        content: 'Could not find user 2! Either you mistyped something, or this user no longer exists.',
+                        ephemeral: true
+                    });
+                } else {
+                    return await interaction.editReply({
+                        content: 'I had an issue accessing the TETR.IO servers! Please try again later.',
+                        ephemeral: true
+                    });
+                }
+            }
+        }
+            
+        console.log(userStats1)
+
+
+
 	},
 };
  
