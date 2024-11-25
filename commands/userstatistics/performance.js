209a7bb7b9bf6d59830831ff7f4d387b473adcf8
erzahoq <154752392+erzahoq@users.noreply.github.com>
2024-11-25 14:02:19 -0800

    performanc inhancin drug
    not finished yet

--- commands/userstatistics/performance.js
+++ commands/userstatistics/performance.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const api = `https://ch.tetr.io/api/`
+const api = `https://ch.tetr.io/api`
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,9 +10,51 @@ module.exports = {
                 .setDescription('The TETR.IO username')
                 .setRequired(true)),
 	async execute(interaction) {
+		let response;
+		
+		let userInfo;
+		let gamemodeInfo;
+		let leagueflowInfo;
+		let skillflowInfo40L;
+		let skillflowInfoBlitz;
 
-        //interaction.reply('tba (tetris battle advanced)') //this is truely one of the most battle advanced of all time
-        
+        	//interaction.reply('tba (tetris battle advanced)') //this is truely one of the most battle advanced of all time
+
+		// get user info at /users/:user
+		response = await fetch(`${api}/users/${user}`);
+            	userInfo = await response.json();
+
+		//check if the API call succeeded
+		if (!userInfo.success) {
+			//check if it failed, and if it did, check if it was the user's fault
+	                if (userInfo.error.msg === "No such user! | Either you mistyped something, or the account no longer exists.") {
+	                    return await interaction.reply({
+	                        content: 'No such user! Either you mistyped something, or this user no longer exists.',
+	                        ephemeral: true
+	                    });
+	                } else {
+			//API fault L
+	                    return await interaction.reply({
+	                        content: 'I had an issue accessing the TETR.IO servers! Please try again later.',
+	                        ephemeral: true
+	                    });
+	                }
+	            }
+		//if it's all okay, then:
+		// get gamemode specific info at /users/:user/summaries
+		response = await fetch(`${api}/users/${user}/summaries`);
+            	gamemodeInfo = await response.json(); 	
+		// get leagueflow info at /labs/leagueflow/:user (for Tetra League)
+		response = await fetch(`${api}/labs/leagueflow/${user}`);
+		leagueflowInfo = await response.json();
+		// get skillflow info at /labs/scoreflow/:user/:gamemode (for 40L and Blitz)
+		response = await fetch(`${api}/labs/scoreflow/${user}/40l`);
+		skillflowInfo40L = await response.json();
+		response = await fetch(`${api}/labs/scoreflow/${user}/blitz`);
+		skillflowInfoBlitz = await response.json();
+		
+
+		
 	},
 };
 
