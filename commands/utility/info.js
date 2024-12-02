7a37262e707c2947b1c41d1b04f19528431789a6
MonkeysWithPie <128157282+MonkeysWithPie@users.noreply.github.com>
2024-12-02 08:28:57 -0600

    update info a bit

--- commands/utility/info.js
+++ commands/utility/info.js
@@ -6,6 +6,10 @@ module.exports = {
 		.setDescription('Gives general information about TetriStats.'),
 	async execute(interaction) {
 
-		await interaction.reply(`um\ntoo lazy to write info rn\nill do that later:tm:`);
+		await interaction.reply({ ephemeral: true, content: 
+			`**TetriStats** is an [open source](https://github.com/erzahoq/TetriStats) discord bot that can fetch stats from [tetr.io](https://tetr.io), created by @erzahoq and @monkeyswithpie.
+			
+			more here maybe` // uhh idk what your plans were, did you want like server count or something? idk you can work on this
+		});
 	},
 };
