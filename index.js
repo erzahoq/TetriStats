8f616f270ce5ef1f7d3ce9c2739374b3a0d847a6
erzahoq <154752392+erzahoq@users.noreply.github.com>
2025-03-24 09:57:33 -0700

    fixed yea ok

--- index.js
+++ index.js
@@ -324,7 +324,7 @@ client.on(Events.InteractionCreate, async interaction => {
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
     status();
-    //checkRdAlerts(); disable rq cause it broke DD:
+    checkRdAlerts(); 
 });
 
 // Log in to Discord with your client's token
