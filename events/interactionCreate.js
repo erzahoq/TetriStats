d815be1c8cd8772f2d85a4f9d3c9f8f6330aed91
MonkeysWithPie <128157282+MonkeysWithPie@users.noreply.github.com>
2026-02-20 09:25:09 -0600

    move interactionCreate to its own file

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,135 +3,85 @@ const {
     DiscordAPIError,
     Events,
     EmbedBuilder,
-    MessageFlags,
-    StringSelectMenuComponent,
+    MessageFlags
 } = require("discord.js");
+const { buildPageButtonRows, formatUsername, formatNumber, formatTime } = require("../helpers/formatters");
+const { getEmoji } = require("../helpers/emojis");
 
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
         try {
-            if (
-                interaction.isChatInputCommand() ||
-                interaction.isMessageContextMenuCommand()
-            ) {
+            if (interaction.isChatInputCommand()) {
                 const command = interaction.client.commands.get(interaction.commandName);
 
                 if (!command) {
-                    console.log(`[ERROR] No command matching ${interaction.commandName} was found.`);
+                    console.error(`No command matching ${interaction.commandName} was found.`);
                     return;
                 }
 
                 await command.execute(interaction);
-            } else if (interaction.isButton()) {
-                if (
-                    interaction.message.interaction &&
-                    interaction.user.id !=
-                    interaction.message.interaction.user.id
-                ) {
-                    return await interaction.reply({
-                        content: "You cannot interact with this!",
-                        flags: MessageFlags.Ephemeral,
+            } else if (interaction.isStringSelectMenu()) {
+                const menuId = interaction.customId;
+
+                // Achievements dropdown
+                if (menuId.startsWith("achselect_")) {
+                    const interactionId = menuId.split("achselect_")[1];
+                    const pageData = interaction.client.pageData.get(interactionId);
+                    if (!pageData) return;
+
+                    const chosen = interaction.values?.[0];
+                    if (!chosen || chosen === "achd_none") return;
+
+
+                    const [, pageStr, itemStr] = chosen.split("_");
+                    const pageIndex = Number(pageStr);
+                    const itemIndex = Number(itemStr);
+
+                    const ach = pageData.pageAchsByPageIndex?.[pageIndex]?.[itemIndex];
+                    if (!ach) return;
+
+                    // make sure not to forgor
+                    pageData.view = "detail";
+                    pageData.lastListPage = pageData.currentPage ?? 0;
+                    pageData.currentPage = pageIndex;
+
+                    const detailEmbed = buildAchievementDetailEmbed(ach, pageData.textPages?.[pageIndex], pageData.username);
+
+                    const deleteRow = buildDeleteRow(interactionId, pageData.ownerId);
+
+                    await interaction.reply({
+                        embeds: [detailEmbed],
+                        components: [deleteRow],
                     });
+                }
+            } else if (interaction.isButton()) {
+                //generic paging system (yay)
+                if (parsePageCustomId(interaction.customId)) {
+                    const handled = await handleGenericPageButton(interaction);
+                    if (handled) return;
                 }
 
                 const buttonId = interaction.customId;
-                const split = buttonId.split(":");
-                const buttonCommand = interaction.client.commands.get(split[0]);
 
-                if (split[1]) {
-                    if (!buttonCommand || !buttonCommand.buttons) {
-                        console.error(
-                            `No command for button ${buttonId} (command ${buttonCommand}) was found.`,
-                            interaction.client
-                        );
-                        return;
-                    } else {
-                        let params = split[1].split("-");
-                        const buttonId = params.shift();
-
-                        await buttonCommand.buttons[buttonId](
-                            interaction,
-                            ...params
-                        );
+                // Achievements delete
+                if (buttonId.startsWith("achdelete")) {
+                    const ownerId = buttonId.split("_")[2];
+                    if (interaction.user.id !== ownerId) {
+                        return await interaction.reply({ content: 'You cannot interact with this!', ephemeral: true });
                     }
-                }
-            } else if (interaction.isStringSelectMenu()) {
-                if (
-                    interaction.message.interaction &&
-                    interaction.user.id !=
-                    interaction.message.interaction.user.id
-                ) {
-                    return await interaction.reply({
-                        content: "You cannot interact with this!",
-                        flags: MessageFlags.Ephemeral,
-                    });
-                }
-                if (!(interaction.component instanceof StringSelectMenuComponent))
-                    return;
 
-                const dropdownId = interaction.component.customId;
-                const split = dropdownId.split(":");
-                const buttonCommand = interaction.client.commands.get(split[0]);
-
-                if (split[1]) {
-                    if (!buttonCommand || !buttonCommand.dropdowns) {
-                        console.error(
-                            `No command for dropdown ${dropdownId} (command ${buttonCommand}) was found.`,
-                            interaction.client
-                        );
-                        return;
-                    } else {
-                        let params = split[1].split("-");
-                        const buttonId = params.shift();
-
-                        await buttonCommand.dropdowns[buttonId](
-                            interaction,
-                            ...params
-                        );
-                    }
-                }
-            } else if (interaction.isAutocomplete()) {
-                const command = interaction.client.commands.get(interaction.commandName);
-
-                if (!command || !command.autocomplete) {
-                    console.error(
-                        `No autocomplete handler for command ${interaction.commandName} was found.`,
-                        interaction.client
-                    );
-                    return await interaction.respond([]);
-                }
-
-                await command.autocomplete(interaction);
-            } else if (interaction.isModalSubmit()) {
-                const modalId = interaction.customId;
-                const split = modalId.split(":");
-                const modalCommand = interaction.client.commands.get(split[0]);
-
-                if (
-                    !modalCommand ||
-                    !modalCommand.modals ||
-                    !modalCommand.modals[split[1]]
-                ) {
-                    console.error(
-                        `No modal handler for modal ${modalId} was found.`,
-                        interaction.client
-                    );
+                    await interaction.deferUpdate();
+                    await interaction.message.delete().catch(() => {}); // in case message already deleted by user, don't care about error
                     return;
                 }
-
-                await modalCommand.modals[split[1]](interaction);
             }
         } catch (error) {
-            if (error instanceof DiscordAPIError && error.code == 10062) {
-                return console.log(
-                    `[INFO] unknown interaction error; thanks, discord`
-                );
-            }
-            if (error instanceof DiscordAPIError && error.code == 10008) {
-                return console.log(
-                    `[INFO] unknown message error; thanks, discord`
-                );
+            // unknown interaction, unknown message
+            // these are usually discord's fault bc of API latency and stuff
+            const ignoredCodes = [10062, 10008];
+            if (error instanceof DiscordAPIError && ignoredCodes.includes(error.code)) {
+                return;
             }
 
             console.error(
@@ -139,8 +89,6 @@ module.exports = {
                 `\nextra info:
     caused by ${interaction.user.username} (${interaction.user.id}) in ${interaction.guild?.name} (${interaction.guild?.id})
     ${error.requestBody && error.requestBody.json && error.requestBody.json.data ? JSON.stringify(error.requestBody.json.data) : "no request body available"}`,
-                interaction.client,
-                error
             );
 
             const reply = {
@@ -148,9 +96,9 @@ module.exports = {
                     new EmbedBuilder()
                         .setTitle("An error occurred!")
                         .setDescription(
-                            `wuh oh, something broke\ndon't worry! the developer has been informed of this failure and will fix this bug ASAP.`
+                            `Something went wrong!`
                         )
-                        .setColor("#ff0000"),
+                        .setColor("#ff5757"),
                 ],
                 flags: MessageFlags.Ephemeral,
             };
@@ -162,9 +110,182 @@ module.exports = {
                         await interaction.reply(reply);
                     }
                 }
-            } catch {
-                console.error("[ERROR] error message sending failed? guh");
-            }
+            } catch {}
         }
     },
 };
+
+
+function buildAchievementDetailEmbed(ach, listEmbed, username) {
+    console.log(ach)
+
+    const achievementMapping = {
+        100: 'issued',
+        1: 'bronze',
+        2: 'silver',
+        3: 'gold',
+        4: 'platinum',
+        5: 'diamond'
+    };
+
+
+    const e = new EmbedBuilder().setColor(listEmbed.data.color || 'ffffff');
+    if (listEmbed?.data?.color) {
+        e.setColor(listEmbed.data.color);
+    }
+
+    const lines = [];
+
+    if (ach.rank != null) {
+        emoji = getEmoji("ach_" + achievementMapping[ach['rank']]);
+    }
+
+    if (ach.category) lines.push(`### __${formatUsername(username)} -> Achievements -> ${ach.name}__`);
+
+    let achText = ""
+
+    //ok this is the same achtext shit
+    //format thing because api silly
+    let displayVal = formatNumber(Math.round(ach.v));
+    if (ach.vt === 2) displayVal = `${formatTime(ach.v)}`
+    else if (ach.vt === 3) displayVal = `${formatTime(-ach.v)}`
+    else if (ach.vt === 4) displayVal = `${formatNumber(ach.v)}m (Floor ${Math.floor(ach.a)})`
+    else if (ach.name === "Guardian Angel") displayVal = `${formatNumber(ach)}m` //fuck you OSK you bitch (jk we love you)
+    else if (ach.vt === 5) displayVal = `Obtained ${formatTime(-ach.v)}`
+    else if (ach.vt === 6) displayVal = formatNumber(-Math.round(ach.v))
+
+    achText += `\n` + getEmoji("ach_" + achievementMapping[ach['rank']])
+
+    achText += ` **${displayVal}** ${ach['object']} \n` // show the main info
+
+    if (ach.nolb) { // if it's issued
+        achText += `Issue ${ach['pos']}/${ach['total']}` 
+    } else {
+        if (ach['pos'] < 100) { // if you're in the top 100 players
+            achText += `**#${ach['pos'] + 1}** in the world`
+        }
+        else {
+            let precision = 2;
+            const achPercentile = ach['pos'] / ach['total'] * 100
+            if (achPercentile < 0.01) precision = 3;
+
+            achText += `**#${ach['pos'] + 1}** in the world (Top ${formatNumber(achPercentile, precision)}%)` // literally just one extra point of precision
+        } 
+    }
+
+    //duo achievement
+    if (ach.x?.ally) {
+        let allyUsername = ach.x.ally.username;
+        achText += `\n With ${formatUsername(allyUsername)}`;
+    }
+
+    //check for attributes and format
+    if (ach.art > -1) achText += "\n" //sorry (???)
+    if (ach.art === 0) {
+        achText += `\n${getEmoji('au')} **UNRANKED** / This achievement does not contribute to your Achievement Rating.`
+    } else if (ach.art === 2) {
+        achText += `\n${getEmoji('ac')} **COMPETITIVE** / This achievement grants extra Achievement Rating to those who place in its Top 100 leaderboard.`
+    }
+    if (ach.hidden) {
+        achText += `\n${getEmoji('ah')} **HIDDEN** / This achievement is only visible to the worthy.`
+    }
+    if (ach.event) {
+        let eventName = ach.event;
+        let extraText = '';
+        if (ach.category === 'legacy') extraText = " It is no longer available."
+        let currentText = 'is part';
+        if (ach.category === 'legacy') currentText = "was part"
+        achText += `\n${getEmoji('ae')} **EVENT** / This achievement ${currentText} of the ${eventName} event.${extraText}`;
+    }
+
+    if (ach.desc) {
+        achText += `\n\n-# *${ach.desc}*`;
+    }
+
+    lines.push(achText);
+
+    e.setDescription(lines.join('\n') || 'No extra info available.');
+
+    return e;
+}
+
+//these should be in a helper file but im lazy so
+
+function buildDeleteRow(interactionId, ownerId) {
+    return new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+            .setCustomId(`achdelete_${interactionId}_${ownerId}`)
+            .setLabel("Delete")
+            .setStyle(ButtonStyle.Secondary)
+    );
+}
+
+// ==== new (!) page system ====
+
+// get custom id commandName:page-key-index
+function parsePageCustomId(customId) {
+    // for example: "league:page-<key>-<pageIndex>"
+    const [commandName, rest] = customId.split(':');
+    if (!commandName || !rest) return null;
+
+    const parts = rest.split('-');
+    if (parts[0] !== 'page') return null;
+    if (parts.length < 3) return null;
+
+    const pageIndexStr = parts[parts.length - 1];
+    const pageIndex = Number(pageIndexStr);
+    if (!Number.isInteger(pageIndex)) return null;
+
+    const key = parts.slice(1, -1).join('-');
+
+    return { commandName, key, pageIndex };
+}
+
+async function handleGenericPageButton(interaction) {
+    const parsed = parsePageCustomId(interaction.customId);
+    if (!parsed) return false;
+
+    const { commandName, key, pageIndex } = parsed;
+
+    const session = interaction.client.pageData.get(key);
+    if (!session) {
+        await interaction.reply({ content: "This menu expired (or I restarted). Run the command again.", ephemeral: true });
+        return true;
+    }
+
+    if (session.commandName !== commandName) {
+        await interaction.reply({ content: "This button doesn't match this message.", ephemeral: true });
+        // this shouldnt ever happen if i did everything correctly but just in case :woomy:
+        return true;
+    }
+
+    // check owner (surely this works this time)
+    if (interaction.user.id !== session.ownerId) {
+        await interaction.reply({ content: "You can't interact with this.", ephemeral: true });
+        return true;
+    }
+
+    if (!session.pages?.[pageIndex]) return true;
+
+    session.currentPage = pageIndex;
+    session.expiresAt = Date.now() + (session.ttlMs ?? 10 * 60 * 1000);
+
+    const rows = buildPageButtonRows({
+        commandName: session.commandName,
+        key,
+        labels: session.labels,
+        activeIndex: pageIndex
+    });
+
+    //let some commands append extra components (e.g. the achievements menu)
+    const extra = typeof session.getExtraComponents === 'function'
+        ? await session.getExtraComponents(pageIndex)
+        : [];
+
+    await interaction.update({
+        embeds: [session.pages[pageIndex]],
+        components: [...rows, ...extra]
+    });
+
+    return true;
+}
